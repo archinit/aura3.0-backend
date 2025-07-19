dd45f8574b1bd91486f1ca4b5dbf213808979ace
ArchitGitHubPro <archit.7537@gmail.com>
2025-07-19 18:47:29 +0530

    updated Schema

---
+++ src/models/Chat.ts
@@ -0,0 +1,55 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IChatMessage {
+    role: "user" | "assistant";
+    content: string;
+    timestamp: Date;
+    metadata?: {
+        technique: string;
+        goal: string;
+        progress: any[];
+    }
+};
+
+export interface IChatSession extends Document {
+    sessionId: string;
+    messages: IChatMessage[];
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const chatMessagesSchema = new Schema<IChatMessage>({
+    role: {
+        type: String,
+        enum: ["user", "assistant"],
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now(),
+    },
+    metadata: {
+        technique: String,
+        goal: String,
+        progress: [Schema.Types.Mixed],
+    },
+});
+
+const chatSessionSchema = new Schema<IChatSession>({
+    sessionId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    messages: [chatMessagesSchema],
+},
+{
+    timestamps: true
+}
+);
+
+export const ChatSession = mongoose.model<IChatSession>("ChatSesion", chatSessionSchema);
