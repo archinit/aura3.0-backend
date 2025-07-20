4d126a84adf8f1a252c2b34e5d2c33284150b3e2
ArchitGitHubPro <archit.7537@gmail.com>
2025-07-20 12:09:48 +0530

    controllers updated

---
+++ src/controllers/activityController.ts
@@ -0,0 +1,35 @@
+import { Activity } from "@/models/Activity";
+import { logger } from "@/utils/logger";
+import { Request, Response } from "express";
+
+export const logActivity = async (req: Request, res: Response) => {
+    try {
+        const { type, name, description, duration } = req.body;
+        const userId = req.user._id;
+
+        if (!userId) {
+            return res.status(401).json({
+                msg: "User not Authenticated"
+            });
+        }
+
+        const activity = new Activity({
+            type,
+            name,
+            description,
+            duration,
+            timestamp: new Date(),
+        });
+
+        await activity.save();
+        logger.info(`Activity logged for user ${userId}`);
+
+        res.status(201).json({
+            success: true,
+            data: activity,
+        });
+
+    } catch (error) {
+        res.status(500).json({ msg: "Internal server error", error });
+    };
+} 
---
+++ src/controllers/moodController.ts
@@ -0,0 +1,27 @@
+import { Mood } from "@/models/Mood";
+import { logger } from "@/utils/logger";
+import { Request, Response } from "express";
+
+export const createMood = async (req: Request, res: Response) =>{
+    try {
+        const { score, note } = req.body;
+        const userId = req.user._id;
+
+        if (!userId) {
+            return res.status(401).json({ msg: "User not Authenticated"});
+        }
+
+        const mood = new Mood({
+            userId,
+            score,
+            note,
+            timestamp: new Date(),
+        });
+
+        await mood.save();
+        logger.info(`Mood logged for user ${userId}`)
+
+    } catch (error) {
+        res.status(500).json({ msg: "Internal server error", error });
+    };
+}
