81eb4026ded8027bfbdd9f08b4afca2e0578a2d4
ArchitGitHubPro <archit.7537@gmail.com>
2025-07-30 18:11:29 +0530

    fix 2

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,13 +1,13 @@
 import { login, logout, register } from "../controllers/authController";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import express from "express";
+import express, { Request, Response } from "express";
 
 const router = express.Router()
 
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", authMiddleware ,logout);
-router.get("/me", authMiddleware, (req, res) => {
+router.get("/me", authMiddleware, (req: Request, res: Response) => {
     res.json({
         user: req.user
     });
